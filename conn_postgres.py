cb56838fddd42ea063b1373059bc71c607b21c0c
Joelcio Rizelo <jrizelo@gmail.com>
2018-05-14 16:12:31 -0300

    teste final

--- conn_postgres.py
+++ conn_postgres.py
@@ -7,7 +7,9 @@ database = 'postgres'
 # Simple routine to run a query on a database and print the results:
 def doQuery( conn ) :
     cur = conn.cursor()
-
+    
+    novo_rec = "insert into emp values ((select max(id) +1 from emp), 'Jose da Silva', '51987655521', '1')"
+    cur.execute(novo_rec)
     cur.execute( "SELECT name, fone FROM emp" )
 
     for name, fone in cur.fetchall() :
@@ -17,4 +19,12 @@ print ("Using psycopg2…")
 import psycopg2
 myConnection = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
 doQuery( myConnection )
+myConnection.commit()
 myConnection.close()
+
+import pgdb
+myConnection1 = pgdb.Connection( host=hostname, user=username, password=password, database=database )
+doQuery( myConnection1 )
+myConnection1.commit()
+myConnection1.close()
+
